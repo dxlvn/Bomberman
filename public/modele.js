4a136979bf714565517c7e70a730f02b0cfaf843
Glitch (hello-express) <none>
2021-04-14 18:28:14 +0000

    🌅✌️ Checkpoint ./public/modele.js:30700191/1260 ./views/index.html:30700191/9

--- public/modele.js
+++ public/modele.js
@@ -1,3 +1,29 @@
+class Personnage {
+  constructor(x, y, size) {
+    this.x = x;
+    this.y = y;
+    this.size = size;
+  }
+  
+  draw() {
+    const canvas = document.getElementById("map");
+    if (!canvas.getContext) {
+      return;
+    }
+
+    // get the context
+    let context = canvas.getContext("2d");
+
+    // set fill and stroke styles
+    context.lineWidth = 2;
+    context.fillStyle = "red";
+    context.strokeStyle = "red";
+
+    // draw a rectangle with fill and stroke
+        context.fillRect(this.x, this.y, this.size, this.size);
+  }
+}
+
 // representation de notre map Bomber's
 class Map {
   constructor(x, y, size_case) {
@@ -42,10 +68,12 @@ class Modele {
   
   //dessiner la map
   draw() {
-    map.draw();
-    perso.draw();
+    this.map.draw();
+    this.perso.draw();
   }
 }
 // appel pour l'afficher
+let perso1 = new Personnage(0, 0, 15);
 let map1 = new Map(10, 10, 20);
-map1.draw();
+let modele = new Modele(map1, perso1);
+modele.draw();
