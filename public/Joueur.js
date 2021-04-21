33a55382314938d6f852d1263360ecdd385d57c6
Glitch (hello-express) <none>
2021-04-21 22:35:42 +0000

    🐬🌂 Checkpoint ./public/Joueur.js:30700497/587 ./public/page.html:30700497/7

--- public/Joueur.js
+++ public/Joueur.js
@@ -8,32 +8,33 @@ class Joueur {
     //Taille du joueur (en pixel)
     this.size = size;
     this.map = m;
+    //this.varm = document.getElementById("image");
   }
   
-  var img = document.getElementById("image");
+  
 
   makeMove(keys) {
     //Regarde les deux dernieres touches enfoncées
     let moveDep = [[0,0],[0,0]]
     let nbKeys =(keys.length > 1) ? 2 : 1;
-    let direction = null;
+    //let direction = null;
     for (let i = 0; i < nbKeys; i++) {
       if (keys[keys.length - i - 1] == "ArrowLeft") {
         moveDep[i][0] = -1;
-        direction = "left";
+        //direction = "left";
       } else if (keys[keys.length - i - 1] == "ArrowRight") {
         moveDep[i][0] = 1;
-        direction = "right";
+        //direction = "right";
       } else if (keys[keys.length - i - 1] == "ArrowUp") {
         moveDep[i][1] = -1;
-        direction = "up";
+        //direction = "up";
       } else if (keys[keys.length - i - 1] == "ArrowDown") {
         moveDep[i][1] = 1;
-        direction = "down";
+        //direction = "down";
       }
     }
     //On fait le délacement de la derniere touche
-    this.move(moveDep[0][0], moveDep[0][1],direction);
+    this.move(moveDep[0][0], moveDep[0][1]);
     //Et si celui de l'avant deniere n'est pas contradictoire
     if(Math.abs(moveDep[0][0])!= Math.abs(moveDep[1][0]) &&
       Math.abs(moveDep[0][1])!= Math.abs(moveDep[1][1])) {
@@ -44,7 +45,7 @@ class Joueur {
 
   //Deplace le joueur dans le x et y indiqué
   //Possibilité d'ajouter un parametre pour la direction
-  move(xDep, yDep, directi) {
+  move(xDep, yDep) {
     //Test de collision
     if (
       //Teste le coin haut gauche
@@ -58,9 +59,6 @@ class Joueur {
     ) {
       this.x += xDep * this.dep;
       this.y += yDep * this.dep;
-      var img = document.getElementById("left");
-    context.drawImage(img,this.x, this.y,this.size, this.size);
-      
     }
   }
 
@@ -72,7 +70,7 @@ class Joueur {
 
     // draw a rectangle with fill and stroke
     //context.fillRect(this.x, this.y, this.size, this.size);
-    
-    context.drawImage(img,this.x, this.y,this.size, this.size);
+    this.varm = document.getElementById("image");
+    context.drawImage(this.varm,this.x, this.y,this.size, this.size);
   }
 }
